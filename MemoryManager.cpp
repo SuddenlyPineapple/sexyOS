b3b600cdd6df718326fc0d66f1a891d5efef7bba
Wojciech Kasperski <wojciech.kasperski97@gmail.com>
2018-10-15 09:04:02 +0200

    First commit

---
+++ MemoryManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Wojtek on 15-Oct-18.
+//
+
+#include "MemoryManager.h"
---
+++ MemoryManager.h
@@ -0,0 +1,15 @@
+//
+// Created by Wojtek on 15-Oct-18.
+//
+
+#ifndef SEXYOS_MEMORYMANAGER_H
+#define SEXYOS_MEMORYMANAGER_H
+
+#include <list>
+
+class MemoryManager {
+
+};
+
+
+#endif //SEXYOS_MEMORYMANAGER_H
