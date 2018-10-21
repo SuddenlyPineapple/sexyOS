5839e6d8290def092b18a38c82ba90eb00ca9dda
Wojciech Kasperski <wojciech.kasperski97@gmail.com>
2018-10-21 21:30:48 +0200

    Adding data set

--- MemoryManager.h
+++ MemoryManager.h
@@ -6,16 +6,29 @@
 #define SEXYOS_MEMORYMANAGER_H
 
 #include <list>
+#include <string>
 //#include "Processes.h"
 
 /* Tablica stronic */
 struct PageTable {
     bool bit;  //Wartość bool'owska sprawdzająca zajętość tablicy w pamięci
     int frame; //Numer ramki w której znajduje się stronica
+
     PageTable(bool bit, int frame);
 };
 
 class MemoryManager {
+private:
+    char RAM[256]; //Pamięć Fizyczna Komputera [256 bajtów]
+
+    //Struktura Stronicy
+    struct Page {
+        char data[16]; //Dane stronicy
+
+        Page(){};
+        Page(std::string data);
+    };
+
 public:
 
     MemoryManager();
